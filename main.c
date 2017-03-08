5f51c6c7fcee5025fbe4c4563c65d0ec0b063d75
Arishenk <i_am_a_celebrity@list.ru>
2017-03-09 01:59:18 +1200

    Проверка данных на корректность

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+int check(int srok, long int summa) {   
+    int k = 0;
+    if ((srok < 0) || (srok > 365) || (summa < 10000))  k = 0;
+    else k = 1;							  
+    return k;
+}
 int main() {
     int srok = 0, l = 0, summa = 0;
     printf("enter the term of the deposit\n");
