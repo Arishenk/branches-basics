f65c14a2058d47391b3f2cd91835ffde9bab9633
Arishenk <i_am_a_celebrity@list.ru>
2017-03-09 01:54:36 +1200

    Ввод данных

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int srok = 0, l = 0, summa = 0;
+    printf("enter the term of the deposit\n");
+    scanf("%d", &srok);
+    printf("enter the deposit bag\n");
+    scanf("%d", &summa);
+    return 0;
+}
